bd0cf0f9729a1069c985b01f909dfcc58e7766a1
Donatien NAMBININTSOA <donatiennambinintsoa@gmail.com>
2022-03-29 18:42:16 +0300

    add build instruction comment into build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-g++ main.cpp -o classy_clash -I./raylib/include -L./raylib/lib -lraylib -Wl,-R./raylib/lib
+g++ main.cpp -o classy_clash -std=c++17 -ferror-limit=1000 -I./raylib/include -L./raylib/lib -lraylib -Wl,-rpath,"raylib/lib" -g
+
+# if Mac M1
+# arch -x86_64 ./build.sh
+
+# else (linux, windows)
+# ./build.sh
